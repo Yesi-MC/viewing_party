fb3a7b922664d7ad9df421801d692531424cbe61
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-03 16:47:13 -0700

    update create action to include flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    new_user = User.create(user_params)
+    flash[:success] = "Welcome, #{new_user.email}"
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
