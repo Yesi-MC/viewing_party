b326b0da656b4858b8b8731aaefd31a293b93168
Yesi Meza <yesi.meza10@gmail.com>
2021-02-03 22:39:11 -0700

    add routes for movies index,search, and details
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+
+  get "/movies", to: "movies#index"
+  get "/movies/search", to: "movies#search"
+  get "/movies/:id", to: "movies#details" 
+  
+  get "/discover", to: "discover#index"
 end
+
+
+
+
+
+
+
