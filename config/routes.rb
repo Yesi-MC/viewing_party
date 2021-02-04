b344931db253feefc9123769c863e245e393c1b1
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-04 14:04:23 -0700

    Make dashboard route nested with users (blank line) Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com> Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  resources :users, only: [:new, :create]
-  resources :dashboard, only: [:index]
+  resources :users, only: [:new, :create] do
+    resources :dashboard, only: [:index]
+  end
   resources :discover, only: [:index]
 end
