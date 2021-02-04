286531b7e756cc68f5a9f317657dcbd76818e058
Sam Yeo <yeosamuel95@gmail.com>
2021-02-03 23:32:21 -0700

    Add test to check if discover movies button directs to proper route

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Dashboard Index' do
   end
   describe 'Initial Visit' do
     it 'can see user email, Discover Movies button, Friends section, and Viewing Parties' do
+      # happy path
       #login first, then get rid of visit action in order to pass authentication
       visit dashboard_index_path
 
@@ -17,6 +18,17 @@ RSpec.describe 'Dashboard Index' do
       within('section#parties') do
         expect(page).to have_content("No current parties going on. Would you like to start one?")
       end
+    end
+  end
+  describe 'Links Directing to Other Pages' do
+    it 'can click on Discover Movies button and route to the Discover Index page' do
+      #happy path
+      # login first, then get rid of visit action in order to pass authentication
+      visit dashboard_index_path
+      click_on "Discover Movies"
+
+      expect(current_path).to eq(discover_index_path)
+      expect(page).to have_content("Discover Movies")
     end
   end
 end
