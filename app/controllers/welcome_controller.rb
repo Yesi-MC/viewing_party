2fe3e5608b0d273ee900614bb6efa96d8cb35854
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-04 18:41:04 -0700

    Add logic to welcome controller to redirect logged in users to dashboard (blank line) Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com> Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    if current_user
+      redirect_to "/users/#{@current_user.id}/dashboard"
+    end
   end
 end
--- spec/features/users/authorization_spec.rb
+++ spec/features/users/authorization_spec.rb
@@ -24,5 +24,21 @@ describe 'User registration' do
       expect(current_path).to eq("/users/#{user.id}/dashboard")
       expect(page).to have_content("Welcome, #{email}")
     end
+    it 'It redirect users that are logged in to the dashboard' do
+      user = User.create(email: 'user@example.com', password: 'password')
+
+      visit root_path
+
+      click_on 'I have an account'
+
+      fill_in :email, with: user.email.upcase
+      fill_in :password, with: user.password
+
+      click_on 'Log In'
+
+      visit root_path
+
+      expect(current_path).to eq("/users/#{user.id}/dashboard")
+    end
   end
 end
