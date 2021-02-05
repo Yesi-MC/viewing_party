da8947533b4ea87649f1cd224cf7cc412d0b39a4
Sam Yeo <yeosamuel95@gmail.com>
2021-02-04 20:53:42 -0700

    Add feature test to check where Discover Movies button directs to
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>

--- spec/features/users/dashboard/index_spec.rb
+++ spec/features/users/dashboard/index_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Dashboard Index' do
   describe 'Dashboard shows all links and buttons' do
+    describe 'Happy Path' do
       it 'can click on a Discover Movies button' do
         visit login_path
         email = "email@gmail.com"
@@ -10,11 +11,8 @@ RSpec.describe 'Dashboard Index' do
         fill_in :email, with: email
         fill_in :password, with: password
         click_on "Log In"
-
-        click_on "Discover Movies"
-
         expect(page).to have_content("Welcome, #{user.email}")
-        expect(page).to have_button("Discover Movies")
+        
         within('section#friends') do
           # User currently has no friends
           expect(page).to have_content("You currently have no friends")
@@ -23,6 +21,11 @@ RSpec.describe 'Dashboard Index' do
           expect(page).to have_content("Feeling like partying?")
           expect(page).to have_link("Click here to start a party!")
         end
+        expect(page).to have_button("Discover Movies")
+        click_on "Discover Movies"
+        
+        expect(current_path).to eq(discover_index_path)
       end
+    end
   end
 end
