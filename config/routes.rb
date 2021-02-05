51636daa57a207ffec7be0190b3055a271003ea8
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-04 18:10:40 -0700

    update dashboard route to handroll (blank line) Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com> Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-
   root 'welcome#index'
-  resources :users, only: [:new, :create] do
-    resources :dashboard, only: [:index]
-  end
+
+  resources :users, only: [:new, :create]
+
   resources :discover, only: [:index]
+
+  get '/users/:id/dashboard', to: 'users/dashboard#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
