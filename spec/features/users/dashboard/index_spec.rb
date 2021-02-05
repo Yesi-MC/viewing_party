b22063282dae84375fdc970614ada9d63f15c969
Sam Yeo <yeosamuel95@gmail.com>
2021-02-04 21:45:34 -0700

    Add test for adding friend who exists in database
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>

--- spec/features/users/dashboard/index_spec.rb
+++ spec/features/users/dashboard/index_spec.rb
@@ -28,6 +28,32 @@ RSpec.describe 'Dashboard Index' do
         
         expect(current_path).to eq(discover_index_path)
       end
+      it 'can find friends and add them as friends' do
+        visit login_path
+        email = "email@gmail.com"
+        password = "test"
+        user = User.create(email: email, password: password)
+        fill_in :email, with: email
+        fill_in :password, with: password
+        click_on "Log In"
+        friend_email = "friend@gmail.com"
+        friend_password = "test1"
+        friend = User.create(email: friend_email, password: friend_password)
+        
+        fill_in :email, with: friend_email
+        click_on "Add Friend"
+
+        expect(user.friends).to eq([friend])
+        expect(current_path).to eq("/users/#{user.id}/dashboard")
+        
+        within('section#friends') do
+          # User currently has no friends
+          expect(page).to_not have_content("You currently have no friends")
+          expect(page).to have_content(friend.email)
+          expect(page).to have_button("Add Friend")
+          expect(page).to have_field(:email)
+        end
+      end
     end
   end
 end
