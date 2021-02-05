d0ca1518b185187c6e8f6d2bcfb8a8905570c3e9
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-04 22:34:30 -0700

    Update files to format according to rubocop

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,4 +1,2 @@
 class DiscoverController < ApplicationController
-  def index
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, require: true
-  has_many :watch_parties
-  has_many :friendships
+  validates :password, presence: true
+  has_many :watch_parties, dependent: :destroy
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
 end
