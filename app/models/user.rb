138591f991a22bb26c7403d2e8a4deb534c601a7
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-04 19:28:04 -0700

    delete line (blank line) Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com> Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-
   has_many :watch_parties
   has_many :friendships
   has_many :friends, through: :friendships
