d59601c28ae681d7f97ae97ca4d480fae5ed7823
Yesi Meza <yesi.meza10@gmail.com>
2021-02-05 21:18:53 -0700

    created api mck tesst
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- spec/features/movie/top_forty_movie_spec.rb
+++ spec/features/movie/top_forty_movie_spec.rb
@@ -1,26 +1,31 @@
 require 'rails_helper'
+require 'webmock/rspec'
+WebMock.enable!
 
 RSpec.describe 'API Connection' do
   describe 'happy path' do
-    it "can see the top 40 rated movies" do
+    it "can see the top 20 rated movies", :vcr do
       user = User.create(email: 'user@example.com', password: 'password')
 
-      # json_response = File.read('spec/fixtures/top_rated_data.json')
+      json_response = File.read('spec/fixtures/top_rated_data.json')
         stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&page=1.json").
-        # with(
-        #   headers: {
-        #  'Accept'=>'*/*',
-        #  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        #  'User-Agent'=>'Faraday v0.15.4',
-        #  'X-Api-Key'=>'opyjcKdEUKllG8P5V15kv3yKKbx1KwkGQwXbfCF3'
-        #   }).
-        to_return(status: 200, body: "")
+        # # with(
+        # #   headers: {
+        # #  'Accept'=>'*/*',
+        # #  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        # #  'User-Agent'=>'Faraday v0.15.4',
+        # #  'X-Api-Key'=>'opyjcKdEUKllG8P5V15kv3yKKbx1KwkGQwXbfCF3'
+        # #   }).
+        to_return(status: 200, body: json_response)
 
-        visit dashboard_path(user.id)
+        visit discover_index_path
 
-        click_button 'Discover Movies'
-
+        click_link "Find Top Rated Movies"
+      
+        expect(current_path).to eq(movies_path)
+        expect(page).to have_content("Gabriel's Inferno Part III")
+        expect(page).to have_content("Your Name.")
+        expect(page).to have_content("City of God")
     end 
-
   end 
 end
