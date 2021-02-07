dc5f4ab5b8286ed91b4d0dbf1f399d47bad50bff
Yesi Meza <yesi.meza10@gmail.com>
2021-02-07 13:10:44 -0700

    created the api connection test with vcr
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- spec/features/movie/top_forty_movie_spec.rb
+++ spec/features/movie/top_forty_movie_spec.rb
@@ -1,31 +1,28 @@
 require 'rails_helper'
-require 'webmock/rspec'
-WebMock.enable!
+# require 'webmock/rspec'
+# WebMock.enable!
 
 RSpec.describe 'API Connection' do
   describe 'happy path' do
-    it "can see the top 20 rated movies", :vcr do
-      user = User.create(email: 'user@example.com', password: 'password')
+    it "can see the top 40 rated movies" do
+      VCR.use_cassette("top_movies") do #creates cassette
+        #L8 creates the casseette- calls api once and saves response in the top_movies.yml  
+        #if we change test then al we need to do is delete the to-movies.yml
+        #it does whatever is in the code.. but it saves response from that api into that file. 
 
-      json_response = File.read('spec/fixtures/top_rated_data.json')
-        stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&page=1.json").
-        # # with(
-        # #   headers: {
-        # #  'Accept'=>'*/*',
-        # #  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        # #  'User-Agent'=>'Faraday v0.15.4',
-        # #  'X-Api-Key'=>'opyjcKdEUKllG8P5V15kv3yKKbx1KwkGQwXbfCF3'
-        # #   }).
-        to_return(status: 200, body: json_response)
+        #we dont need a webmock when we use vcr. the only thing we need is the gem-but NO webmock response. 
+        
+        user = User.create(email: 'user@example.com', password: 'password')
 
         visit discover_index_path
 
-        click_link "Find Top Rated Movies"
-      
+        click_link "Find Top Rated Movies" #this action calls an api so then it saves the reponse from this action into the to-movies file 
+    
         expect(current_path).to eq(movies_path)
         expect(page).to have_content("Gabriel's Inferno Part III")
         expect(page).to have_content("Your Name.")
         expect(page).to have_content("City of God")
+      end 
     end 
   end 
 end
