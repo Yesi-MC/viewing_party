77d585504334030665e032821bc7df6b8cddc64c
Yesi Meza <yesi.meza10@gmail.com>
2021-02-08 16:15:59 -0700

    created sad path test for search api
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- spec/features/movie/search_movies_spec.rb
+++ spec/features/movie/search_movies_spec.rb
@@ -9,14 +9,16 @@ RSpec.describe 'API Connection' do
 
         visit discover_index_path
         fill_in "movie", with: "Untitled Spy Kids Reboot"
-
+       
         click_button "Search"
+
     
         expect(current_path).to eq(movies_search_path)
         expect(page).to have_content("Untitled Spy Kids Reboot")
         expect(page).to have_content("Voter Average: 0")
       end
     end
+    describe 'search for nonexistent' do 
     it 'can search for a nonexistent movie and show 0 results' do
       VCR.use_cassette("non_existent_movies") do
     
@@ -33,41 +35,25 @@ RSpec.describe 'API Connection' do
         expect(page).not_to have_content(not_a_movie)
       end
     end
-    describe 'sad path' do 
-      it "can display error message when no movie is searched" do 
-      # user = User.create(email: 'user@example.com', password: 'password')
-      json_response = File.read('spec/fixtures/movie_search_data.json')
-        stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['movie_api_key']}&query= ").
-        to_return(status: 422, body: json_response)
-        
+  end 
+end
+  describe 'sad path' do 
+      it 'can search for a nonexistent movie and show 0 results' do
+      VCR.use_cassette("400 status") do
+    
+        user = User.create(email: 'user@example.com', password: 'password')
+
         visit discover_index_path
 
+   
         fill_in "movie", with: ""
-        click_button "Search"
 
-        # expect(current_path).to eq(movies_search_path)
-        expect(page).to have_content("Please enter a Movie Title")
-        expect(current_path).to eq(movies_search_path)
+        
+        click_button "Search"
+      
+        expect(page).to have_content("Please Enter a Movie Title")
+        expect(current_path).to eq(discover_index_path)
       end 
     end
   end
-  # describe 'sad path' do 
-      # it 'can search for a nonexistent movie and show 0 results' do
-      # VCR.use_cassette("400 status") do
-    
-      #   user = User.create(email: 'user@example.com', password: 'password')
-
-      #   visit discover_index_path
-
-      #   # not_a_movie = 
-      #   fill_in "movie", with: ""
-
-        
-      #   click_button "Search"
-      #   expect(current_path).to eq(movies_search_path)
-      #   expect(page).to have_content("Please enter a Movie Title")
-      #   expect(current_path).to eq(movies_search_path)
-      # end 
-    # end
-  # end
 end
