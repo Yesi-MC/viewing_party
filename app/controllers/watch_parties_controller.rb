5fb440dd58e067c246eae81a131dc44501f70e36
Sam Yeo <yeosamuel95@gmail.com>
2021-02-09 11:29:50 -0700

    Change user variable to friends in controller
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>
    Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- app/controllers/watch_parties_controller.rb
+++ app/controllers/watch_parties_controller.rb
@@ -1,5 +1,6 @@
 class WatchPartiesController < ApplicationController
   def new
+    @friends = current_user.friends
     @watch_party = WatchParty.new(movie_title: session[:title], duration: session[:runtime])
   end
 end
