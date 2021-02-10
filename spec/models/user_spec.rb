e9c2d06a2e2e36a917442aa26b161425bd886327
Sam Yeo <yeosamuel95@gmail.com>
2021-02-10 09:00:21 -0700

    Create test for party_host method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe User do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
   end
+  describe 'instance methods' do
+    it 'party_host' do
+      user = User.create(email: "user@user.com", password: "password")
+      watch_party_1 = user.watch_parties.create(user_id: user.id, movie_title: "Test", date: "Today", time: "10:00 AM", duration: 20)
+    
+      expect(user.party_host).to eq([watch_party_1])
+
+      watch_party_2 = user.watch_parties.create(user_id: user.id, movie_title: "Test1", date: "Today1", time: "11:00 AM", duration: 201)
+
+      expect(user.party_host).to eq([watch_party_1, watch_party_2])
+    end
+  end
 end
