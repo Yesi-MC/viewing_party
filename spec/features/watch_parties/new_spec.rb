955f828924eb41fc2e4dec8719c9ff12249ad1e2
Yesi Meza <yesi.meza10@gmail.com>
2021-02-10 14:47:08 -0700

    created test for sad path to create viewing party

--- spec/features/watch_parties/new_spec.rb
+++ spec/features/watch_parties/new_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'New Party Page' do
             fill_in "email", with: user.email
             fill_in "password", with: user.password
             click_on "Log In"
-            # require 'pry'; binding.pry
+  
             click_on "Discover Movies"
             
             fill_in "movie", with: "Untitled Spy Kids Reboot"
@@ -27,7 +27,7 @@ RSpec.describe 'New Party Page' do
 
             click_on "Create a Viewing Party for Movie"
 
-            # fill_in "watch_party[time]", with: "10:00 AM"
+            fill_in "watch_party[time]", with: "10:00 AM"
             fill_in "watch_party[date]", with: "03-03-2021"
             fill_in "watch_party[duration]", with: "120"
 
@@ -56,6 +56,40 @@ RSpec.describe 'New Party Page' do
     end
   end
   describe 'Sad Path' do
+    it 'cannot choose duration less than the runtime and a past date' do
+       VCR.use_cassette("search_movies") do
+          VCR.use_cassette("spy_kids_details") do
+            user = User.create(email: 'user@example.com', password: '1234')
+            friend = User.create(email: 'friend@friend.com', password: '1234')
+            other_friend = User.create(email: 'other_friend@friend.com', password: '1234')
+            Friendship.create(user_id: user.id, friend_id: friend.id)
+            Friendship.create(user_id: user.id, friend_id: other_friend.id)
 
+            visit login_path
+
+            fill_in "email", with: user.email
+            fill_in "password", with: user.password
+            click_on "Log In"
+          
+            click_on "Discover Movies"
+            
+            fill_in "movie", with: "Untitled Spy Kids Reboot"
+            click_on "Search"
+         
+            click_on "Untitled Spy Kids Reboot"
+
+            click_on "Create a Viewing Party for Movie"
+
+            fill_in "watch_party[time]", with: "10:00 AM"
+            fill_in "watch_party[date]", with: "02-02-2020"
+            fill_in "watch_party[duration]", with: "2"
+
+            click_on "Create Party"
+          
+            expect(current_path).to eq(new_watch_party_path(user))
+            expect(page).to have_content("Update failed")
+        end 
+      end
+    end
   end
 end
