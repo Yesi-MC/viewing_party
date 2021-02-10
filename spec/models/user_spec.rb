20106f4348755376e8190321bd36c431f54f8c79
Sam Yeo <yeosamuel95@gmail.com>
2021-02-10 13:35:35 -0700

    Add test for parties_invited_to method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe User do
       watch_party_1 = user.watch_parties.create(user_id: user.id, movie_title: "Test", date: "Today", time: "10:00 AM", duration: 20)
       watch_party_2 = user.watch_parties.create(user_id: user.id, movie_title: "Test1", date: "Today1", time: "11:00 AM", duration: 201)
 
-      expect(user.dates_of_all_watch_parties).to eq(["Today", "Today1"])
+      expect(user.dates_of_all_my_hosted_watch_parties).to eq(["Today", "Today1"])
     end
     it '#dates_of_all_watch_parties' do
       user = User.create(email: "user@user.com", password: "password")
@@ -35,6 +35,21 @@ RSpec.describe User do
 
       expect(user.any_same_date_party?("Today")).to eq(true)
       expect(user.any_same_date_party?("Today1")).to eq(false)
+    end
+    it 'parties_invited_to' do
+      user = User.create(email: "user@user.com", password: "password")
+      attendee_1 = User.create(email: "user1@user.com", password: "password")
+      attendee_2 = User.create(email: "user2@user.com", password: "password")
+
+      watch_party_1 = user.watch_parties.create(user_id: user.id, movie_title: "Test", date: "Today", time: "10:00 AM", duration: 20)
+      watch_party_2 = user.watch_parties.create(user_id: user.id, movie_title: "Test1", date: "Today1", time: "11:00 AM", duration: 201)
+
+      Guest.create(watch_party_id: watch_party_1.id, invitee_id: attendee_1.id)
+      Guest.create(watch_party_id: watch_party_2.id, invitee_id: attendee_1.id)
+
+      expect(attendee_1.parties_invited_to).to eq([watch_party_1, watch_party_2])
+      expect(attendee_2.parties_invited_to).to eq([])
+
     end
   end
 end
