ce1c8f4635d072b20d935e94a1d9e09e55362286
Yesi Meza <yesi.meza10@gmail.com>
2021-02-10 00:05:03 -0700

    added validation numericality of

--- app/models/watch_party.rb
+++ app/models/watch_party.rb
@@ -1,6 +1,8 @@
 class WatchParty < ApplicationRecord
   validates :date, :time, :duration, :movie_title, presence: true
-  validates :duration, numericality: { only_integer: true }
+  # validates :duration, numericality: { only_integer: true }
+  validates_numericality_of :duration, :greater_than => 0
+
 
   belongs_to :user
   has_many :guests, dependent: :destroy
