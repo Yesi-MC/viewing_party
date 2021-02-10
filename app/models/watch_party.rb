b1b7f4afaddbfa1820eb003f65f3cf434f8470f1
Yesi Meza <yesi.meza10@gmail.com>
2021-02-09 17:58:30 -0700

    removed the requirement of more than 0
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- app/models/watch_party.rb
+++ app/models/watch_party.rb
@@ -1,6 +1,6 @@
 class WatchParty < ApplicationRecord
   validates :date, :time, :duration, :movie_title, presence: true
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+  validates :duration, numericality: { only_integer: true }
 
   belongs_to :user
   has_many :guests, dependent: :destroy
