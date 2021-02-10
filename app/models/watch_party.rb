d23891b0c88e8e030aaafdc92e425d8e4ccee8eb
Yesi Meza <yesi.meza10@gmail.com>
2021-02-10 12:42:06 -0700

    changed validation to validate numericality

--- app/models/watch_party.rb
+++ app/models/watch_party.rb
@@ -1,6 +1,5 @@
 class WatchParty < ApplicationRecord
   validates :date, :time, :duration, :movie_title, presence: true
-  # validates :duration, numericality: { only_integer: true }
   validates_numericality_of :duration, :greater_than => 0
 
 
