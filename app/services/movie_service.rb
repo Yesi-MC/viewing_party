c8deebfe3b198fcc0870640ade5dbe4a6351d9b9
Yesi Meza <yesi.meza10@gmail.com>
2021-02-10 12:40:11 -0700

    removed comments

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,9 @@
 class MovieService
   class << self
     def top_rated_movies
-      # conn = Faraday.new("https://api.themoviedb.org") #the connection is the URL
       response = conn.get("/3/movie/top_rated?")
       response2 = conn.get("/3/movie/top_rated?&page=2")
-      # data = JSON.parse(response.body, symbolize_names: true )
       data = parse_data(response)
-      # data2 = JSON.parse(response2.body, symbolize_names: true )
       data2 = parse_data(response2)
       data[:results].concat(data2[:results])
     end
