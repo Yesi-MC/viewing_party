0bbe7eaac2a8e66921ea261a874e4d3a9727833f
Sam Yeo <yeosamuel95@gmail.com>
2021-02-10 13:35:54 -0700

    Add method parties_invited_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,23 @@ class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
 
+  # Methods for Party Host Users
   def party_host
     watch_parties
   end
 
-  def dates_of_all_watch_parties
+  def dates_of_all_my_hosted_watch_parties
     party_host.select("watch_parties.date").pluck(:date)
   end
 
   def any_same_date_party?(new_party_date)
-    return true if dates_of_all_watch_parties.include?(new_party_date)
+    return true if dates_of_all_my_hosted_watch_parties.include?(new_party_date)
     false
+  end
+
+  # Methods for Guest Users
+
+  def parties_invited_to
+    joins(:guests).where("guests.invitee_id = ?", self.id)
   end
 end
