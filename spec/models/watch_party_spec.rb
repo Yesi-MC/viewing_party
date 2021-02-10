189bc81881b1fb0fd21f4504be41530c3297b815
Yesi Meza <yesi.meza10@gmail.com>
2021-02-09 17:59:21 -0700

    removed the validation of greater than 0 duration
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: V Arruda  <69529052+nessaarruda@users.noreply.github.com>

--- spec/models/watch_party_spec.rb
+++ spec/models/watch_party_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe WatchParty do
     it { should validate_presence_of :date }
     it { should validate_presence_of :time }
     it { should validate_presence_of :duration }
-    it { should validate_numericality_of(:duration).is_greater_than(0) }
+    it { should validate_numericality_of(:duration) }
     it { should validate_presence_of :movie_title }
   end
 end
