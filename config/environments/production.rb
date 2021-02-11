052c76d40b0043f38c520783273085316b7614c6
Sam Yeo <yeosamuel95@gmail.com>
2021-02-11 05:30:03 -0700

    Change production configs to different default url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,18 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { host: 'fun-movie-night.herokuapp.com' }
+  #config.action_mailer.default_options = { host: 'localhost:3000 '}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'afunmovienight@gmail.com',
+    password: ENV['gmail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
