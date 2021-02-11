3022bade3a5c3dd0cad7d6915388d490914d5538
Sam Yeo <yeosamuel95@gmail.com>
2021-02-11 05:09:52 -0700

    Add delivery errors for action_mailer

--- spec/models/watch_party_spec.rb
+++ spec/models/watch_party_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe WatchParty do
     it { should validate_presence_of :movie_title }
   end
   describe 'instance methods' do
-    it 'valid_party' do
+    it '#valid_party' do
       user = User.create(email: "email@email.com", password: "password")
       watch_party_1 = WatchParty.create(user_id: user.id, movie_title: "Test", date: "09-02-2021", time: "10:00 AM", duration: 20)
 
@@ -23,6 +23,8 @@ RSpec.describe WatchParty do
       watch_party_2 = WatchParty.create(user_id: user.id, movie_title: "Test", date: "09-02-2030", time: "10:00 AM", duration: 20)
 
       expect(watch_party_2.valid_party(watch_party_2.date)).to eq(true)
+    end
+    xit '#valid_party_time' do
 
     end
   end
