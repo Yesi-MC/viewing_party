fef7e82907b9d1e6d6d1682bd251ee9f203a3d6f
Yesi Meza <yesi.meza10@gmail.com>
2021-02-10 17:33:06 -0700

    removed comments from the movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,5 @@
 class MoviesController < ApplicationController
   def index
-
-
-    # Top 40 movies API 
-    # (top rated) will go to the index views
-    # conn = Faraday.new("https://api.themoviedb.org") #the connection is the URL (303)
-    # response = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key_for_fun']}") #this is the URI (123-45678)
-    # response2 = conn.get("/3/movie/top_rated?api_key=#{ENV['movie_api_key']}&page=2") #this is the URI
-    # data = JSON.parse(response.body, symbolize_names: true )
-    # data2 = JSON.parse(response2.body, symbolize_names: true )
-    # @movies = data[:results].concat(data2[:results])
-    # require 'pry'; binding.pry
-
-
     @movies = MovieFacade.call_api_for_all_top_rated_movies
   end
 
@@ -30,16 +17,5 @@ class MoviesController < ApplicationController
     @movie_details = MovieFacade.api_movie_details(params[:id])
     session[:title] = @movie_details.title
     session[:runtime] = @movie_details.runtime
-    # conn = Faraday.new("https://api.themoviedb.org")
-    # response = conn.get("/3/movie/#{movie_id}?api_key=#{ENV['movie_api_key']}")
-    # @movie = JSON.parse(response.body, symbolize_names: true )
-
-    # response2 = conn.get("/3/movie/#{movie_id}/credits?api_key=#{ENV['movie_api_key']}")
-    # data = JSON.parse(response2.body, symbolize_names: true )
-    # @actors = data[:cast][0..9]
-
-    # response3 = conn.get("/3/movie/#{movie_id}/reviews?api_key=#{ENV['movie_api_key']}")
-    # data_review = JSON.parse(response3.body, symbolize_names: true )
-    # @reviews = data_review[:results]
   end
 end
