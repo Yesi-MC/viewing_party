db3fd77012bff0ecaf8ea608c52062b328f1baef
Sam Yeo <yeosamuel95@gmail.com>
2021-02-11 04:49:57 -0700

    Add email configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.action_mailer.default_options = { host: 'localhost:3000 '}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'afunmovienight@gmail.com',
+    password: ENV['gmail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
