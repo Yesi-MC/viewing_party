e212fae8a02bb5b5cea01dbd0c34d28b5a8ee27e
Sam Yeo <yeosamuel95@gmail.com>
2021-02-11 04:50:38 -0700

    Add extra config for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'afunmovienight@gmail.com',
+  password: ENV['gmail_password'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
